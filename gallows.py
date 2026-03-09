9ecef748f162265fd933f02a1904c25aedc8ed73
Arthur290795 <arturche_k@mail.ru>
2026-03-09 15:25:19 +0300

    замена рекурсии на цикл, повторно вводимое слово теперь не ошибка, изменение нейминга методов

--- gallows.py
+++ gallows.py
@@ -1,6 +1,7 @@
 """
 Модуль с классом Gallows - основным классом приложения
 """
+
 import unicodedata
 from random import choice
 from config import Config
@@ -9,21 +10,22 @@ from pictures import GALLOWS_BY_ATTEMPTS
 
 class Gallows:
     """Основной класс игры "Виселица"
-    
+
     Управляет игровым процессом: загадывает слово, принимает буквы,
     отслеживает прогресс и отображает состояние игры.
-    
+
     Attributes:
         word (str): Загаданное слово
-        word_coding (list[str]): Текущее состояние отгаданного слова (буквы и "_")
+        word_state (list[str]): Текущее состояние отгаданного слова (буквы и "_")
         attempts (int): Оставшееся количество попыток
-        unsuccess_letters (list[str]): Список неверно угаданных букв
+        incorrect_letters (list[str]): Список неверно угаданных букв
     """
+
     def __init__(self):
         self.word = self._get_word()
-        self.word_coding = ["_"] * len(self.word)
+        self.word_state = ["_"] * len(self.word)
         self.attempts = Config.MAX_ATTEMPTS
-        self.unsuccess_letters = []
+        self.incorrect_letters = []
         self._greeting()
         self.play()
 
@@ -45,35 +47,35 @@ class Gallows:
         print("-----------------------------")
 
     def play(self) -> None:
-        self._print_info()
-        letter = input("Угадай букву которая есть в слове:")
-        if self._is_valid_letter(letter):
-            if letter.lower().strip() and letter.lower() in self.word:
-                self.success(letter)
+        while self.word_state.count("_") > 0 and self.attempts >= 0:
+            self._print_info()
+            letter = input("Угадай букву которая есть в слове:")
+            if self._is_valid_letter(letter):
+                if self._was_letter_used(letter):
+                    print(f'Ты уже вводил "{letter}" и он оказался не правильным, соберись!')
+                else:
+                    if letter.lower().strip() and letter.lower() in self.word:
+                        self.process_correct_letter(letter)
+                    else:
+                        self.process_incorrect_letter(letter)
             else:
-                self.unsuccess(letter)
-        else:
-            print("Ты ввел не валидный символ(ы), попробуй еще раз!", end="\n\n")
-            self.play()
+                print("Ты ввел не валидный символ(ы), попробуй еще раз!", end="\n\n")
 
-    def success(self, letter: str) -> None:
+    def process_correct_letter(self, letter: str) -> None:
         print("Ты угадал!", end="\n\n")
         for i, char in enumerate(self.word, start=0):
             if char == letter.lower():
-                self.word_coding[i] = char
+                self.word_state[i] = char
 
-        if self.word_coding.count("_") == 0:
+        if self.word_state.count("_") == 0:
             self._congratulate()
             return
-        self.play()
 
-    def unsuccess(self, letter: str) -> None:
-        self.unsuccess_letters.append(letter.lower())
-        print("Ты ошибся!", end="\n\n")
+    def process_incorrect_letter(self, letter: str) -> None:
+        self.incorrect_letters.append(letter.lower())
         self.attempts -= 1
         try:
             self._draw_gallows()
-            self.play()
         except KeyError:
             print("К сожалению ты проиграл!")
             print(f"Я загадал слово: {self.word}")
@@ -88,6 +90,9 @@ class Gallows:
     def _is_valid_letter(self, value: str) -> bool:
         return len(value) == 1 and "CYRILLIC" in unicodedata.name(value)
 
+    def _was_letter_used(self, letter: str) -> bool:
+        return letter.lower() in self.incorrect_letters
+
     def _get_word(self) -> str:
         words_file_path = Config.WORDS_FILE_PATH
         with open(words_file_path, "r", encoding="utf-8") as file:
@@ -95,9 +100,9 @@ class Gallows:
         return choice(words)
 
     def _print_info(self) -> None:
-        if self.unsuccess_letters:
+        if self.incorrect_letters:
             print(
-                f"Неправильные буквы: {', '.join(self.unsuccess_letters)}", end="\n\n"
+                f"Неправильные буквы: {', '.join(self.incorrect_letters)}", end="\n\n"
             )
         print(f"Осталось попыток: {self.attempts + 1}", end="\n\n")
-        print("".join(self.word_coding), end="\n\n")
+        print("".join(self.word_state), end="\n\n")
